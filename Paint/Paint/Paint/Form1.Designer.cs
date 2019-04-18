637751028328bb9e6e22bc37cd06c506a2aab7ad
Anastassiya Nikitina <47048850+alexandrovva@users.noreply.github.com>
2019-04-18 18:21:24 +0600

    Add files via upload

---
+++ Paint/Paint/Paint/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Location = new System.Drawing.Point(704, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 45);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox2.Location = new System.Drawing.Point(115, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 45);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.color_Clicked);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Aqua;
+            this.pictureBox4.Location = new System.Drawing.Point(172, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 45);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.color_Clicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 45);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.color_Clicked);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox3.Location = new System.Drawing.Point(59, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 45);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.color_Clicked);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Location = new System.Drawing.Point(424, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(275, 46);
+            this.panel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.thickness_Clicked);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(224, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 46);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.thickness_Clicked);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(56, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.thickness_Clicked);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(168, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.thickness_Clicked);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(112, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.thickness_Clicked);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(939, 582);
+            this.panel1.TabIndex = 0;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.White;
+            this.button13.Location = new System.Drawing.Point(167, 11);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(68, 45);
+            this.button13.TabIndex = 9;
+            this.button13.Text = "Text";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.text_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.White;
+            this.button12.Location = new System.Drawing.Point(876, 327);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(50, 46);
+            this.button12.TabIndex = 8;
+            this.button12.Text = "St";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.star_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.White;
+            this.button11.Location = new System.Drawing.Point(21, 11);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(66, 45);
+            this.button11.TabIndex = 7;
+            this.button11.Text = "Brush";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.brush_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.White;
+            this.button10.Location = new System.Drawing.Point(875, 274);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(52, 47);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "Tri";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.triangle_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(876, 217);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 51);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "Elli";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.elli_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(93, 11);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(68, 45);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Eraser";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.eraser_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(875, 162);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(52, 49);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Rec";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.rec_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(876, 89);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 50);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Pen";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.pen_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(857, 515);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox5_Paint);
+            this.pictureBox5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.pictureBox5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.pictureBox5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 582);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ Paint/Paint/Paint/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Graphics gBitmap;
+        Bitmap bitmap;
+        Pen pen, eraser_pen;
+        Rectangle rec, elli;
+        string tool;
+        int x, y, triangle_x, triangle_y, star_x, star_y, text_x, text_y; 
+        int eraser_width = 1, eraser_height = 1;   
+        bool moving = false;
+        Queue<Point> q = new Queue<Point>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+            bitmap = new Bitmap(pictureBox5.Width, pictureBox5.Height);
+            gBitmap = Graphics.FromImage(bitmap);
+            pictureBox5.Image = bitmap;
+
+            pen = new Pen(Color.Black, 1);
+            eraser_pen = new Pen(Color.White, 1);
+        }
+
+        private void color_Clicked(object sender, EventArgs e)
+        {
+            PictureBox p = sender as PictureBox;
+            pen.Color = p.BackColor;
+        }
+
+        private void thickness_Clicked(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (tool == "pen" || tool == "rectangle")
+            {
+                pen.Width = float.Parse(btn.Text);
+            }
+            if (tool == "eraser")
+            {
+                eraser_width = int.Parse(btn.Text);
+                eraser_height = int.Parse(btn.Text);
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X;
+            y = e.Y;
+
+            if (tool == "brush")
+            {
+                Color color = bitmap.GetPixel(e.X, e.Y);
+                q.Enqueue(e.Location);
+                Point p;
+                int a, b, c, d;
+                while (q.Count != 0)
+                {
+                    p = q.Dequeue();
+                    bitmap.SetPixel(p.X, p.Y, pen.Color);
+                    a = p.X + 1;
+                    b = p.X - 1;
+                    c = p.Y + 1;
+                    d = p.Y - 1;
+
+                    if (bitmap.GetPixel(a, p.Y) == color && !q.Contains(new Point(a, p.Y)) && a != pictureBox1.Width - 1) q.Enqueue(new Point(a, p.Y));
+                    if (bitmap.GetPixel(b, p.Y) == color && !q.Contains(new Point(b, p.Y)) && b != 0) q.Enqueue(new Point(b, p.Y));
+                    if (bitmap.GetPixel(p.X, c) == color && !q.Contains(new Point(p.X, c)) && c != pictureBox1.Height - 1) q.Enqueue(new Point(p.X, c));
+                    if (bitmap.GetPixel(p.X, d) == color && !q.Contains(new Point(p.X, d)) && d != 0) q.Enqueue(new Point(p.X, d));
+                }
+                if (tool == "text")
+                {
+                    text_x = e.X;
+                    text_y = e.Y;
+                }
+                pictureBox5.Refresh();
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && tool == "pen")
+            {
+                gBitmap.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+                pictureBox5.Refresh();
+            }
+            if (moving && tool == "rectangle")
+            {
+                int x1 = Math.Min(x, e.X);
+                int y1 = Math.Min(y, e.Y);
+                int x2 = Math.Max(x, e.X) - Math.Min(x, e.X);
+                int y2 = Math.Max(y, e.Y) - Math.Min(y, e.Y);
+                rec = new Rectangle(x1, y1, x2, y2);
+                pictureBox5.Refresh();
+            }
+            if (moving && tool == "ellipse")
+            {
+                int x1 = Math.Min(x, e.X);
+                int y1 = Math.Min(y, e.Y);
+                int x2 = Math.Max(x, e.X) - Math.Min(x, e.X);
+                int y2 = Math.Max(y, e.Y) - Math.Min(y, e.Y);
+                elli = new Rectangle(x1, y1, x2, y2);
+                pictureBox5.Refresh();
+            }
+            if (moving && tool == "triangle") {
+                triangle_x = e.X;
+                triangle_y = e.Y;
+                pictureBox5.Refresh();
+            }
+            if (moving && tool == "star")
+            {
+                star_x = e.X;
+                star_y = e.Y;
+                pictureBox5.Refresh();
+            }
+            if (moving && tool == "eraser")
+            {
+                gBitmap.DrawRectangle(eraser_pen, x, y, eraser_width, eraser_height);
+                SolidBrush brush = new SolidBrush(Color.White);
+                gBitmap.FillRectangle(brush, new Rectangle(x, y, eraser_width, eraser_height));
+                x = e.X;
+                y = e.Y;
+                pictureBox5.Refresh();
+            }
+        }
+
+        private void pictureBox5_Paint(object sender, PaintEventArgs e)
+        {
+            if (tool == "rectangle")
+                e.Graphics.DrawRectangle(pen, rec);
+            if (tool == "ellipse")
+                e.Graphics.DrawEllipse(pen, elli);
+            if (tool == "triangle")
+            {
+                e.Graphics.DrawLine(pen, (x + triangle_x) / 2, Math.Min(y, triangle_y), Math.Min(x, triangle_x), Math.Max(y, triangle_y));
+                e.Graphics.DrawLine(pen, (x + triangle_x) / 2, Math.Min(y, triangle_y), Math.Max(x, triangle_x), Math.Max(y, triangle_y));
+                e.Graphics.DrawLine(pen, Math.Min(x, triangle_x), Math.Max(y, triangle_y), Math.Max(x, triangle_x), Math.Max(y, triangle_y));
+            }
+            if (tool == "star")
+            {
+                e.Graphics.DrawLine(pen, (x + star_x) / 2, Math.Min(y, star_y), Math.Min(x, star_x), Math.Max(y, star_y));
+                e.Graphics.DrawLine(pen, (x + star_x) / 2, Math.Min(y, star_y), Math.Max(x, star_x), Math.Max(y, star_y));
+                e.Graphics.DrawLine(pen, Math.Min(x, star_x)-10, (y + star_y) / 2, Math.Max(x, star_x), Math.Max(y, star_y));
+                e.Graphics.DrawLine(pen, Math.Min(x, star_x)-10, (y + star_y) / 2, Math.Max(x, star_x)+10, (y + star_y) / 2);
+                e.Graphics.DrawLine(pen, Math.Max(x, star_x)+10, (y + star_y) / 2, Math.Min(x, star_x), Math.Max(y, star_y));
+            }
+        }
+
+        private void text_Click(object sender, EventArgs e)
+        {
+            tool = "text";
+        }
+
+        private void pen_Click(object sender, EventArgs e)
+        {
+            tool = "pen";
+        }
+
+        private void brush_Click(object sender, EventArgs e)
+        {
+            tool = "brush";
+        }
+
+        private void triangle_Click(object sender, EventArgs e)
+        {
+            tool = "triangle";
+        }
+
+        private void elli_Click(object sender, EventArgs e)
+        {
+            tool = "ellipse";
+        }
+
+        private void eraser_Click(object sender, EventArgs e)
+        {
+            tool = "eraser";
+        }
+
+        private void rec_Click(object sender, EventArgs e)
+        {
+            tool = "rectangle";
+        }
+
+        private void star_Click(object sender, EventArgs e)
+        {
+            tool = "star";
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            if (tool == "rectangle")
+                gBitmap.DrawRectangle(pen, rec);
+            if (tool == "ellipse")
+                gBitmap.DrawEllipse(pen, elli);
+            if (tool == "triangle")
+            {
+                gBitmap.DrawLine(pen, (x + triangle_x) / 2, Math.Min(y, triangle_y), Math.Min(x, triangle_x), Math.Max(y, triangle_y));
+                gBitmap.DrawLine(pen, (x + triangle_x) / 2, Math.Min(y, triangle_y), Math.Max(x, triangle_x), Math.Max(y, triangle_y));
+                gBitmap.DrawLine(pen, Math.Min(x, triangle_x), Math.Max(y, triangle_y), Math.Max(x, triangle_x), Math.Max(y, triangle_y));
+            }
+            if (tool == "star")
+            {
+                gBitmap.DrawLine(pen, (x + star_x) / 2, Math.Min(y, star_y), Math.Min(x, star_x), Math.Max(y, star_y));
+                gBitmap.DrawLine(pen, (x + star_x) / 2, Math.Min(y, star_y), Math.Max(x, star_x), Math.Max(y, star_y));
+                gBitmap.DrawLine(pen, Math.Min(x, star_x)-10, (y + star_y) / 2, Math.Max(x, star_x), Math.Max(y, star_y));
+                gBitmap.DrawLine(pen, Math.Min(x, star_x)-10, (y + star_y) / 2, Math.Max(x, star_x)+10, (y + star_y) / 2);
+                gBitmap.DrawLine(pen, Math.Max(x, star_x)+10, (y + star_y) / 2, Math.Min(x, star_x), Math.Max(y, star_y));
+            }
+            if (tool == "text")
+            {
+                text_x = e.X;
+                text_y = e.Y;
+                TextBox tb = new TextBox();
+                tb.Name = "textBox1";
+                tb.Text = " ";
+                tb.BorderStyle = BorderStyle.None;
+                tb.Location = new Point(text_x, text_y);
+                tb.Size = new Size(100, 30);
+                tb.BackColor = Color.White;
+                pictureBox5.Controls.Add(tb);
+            }
+        }
+    }
+}
